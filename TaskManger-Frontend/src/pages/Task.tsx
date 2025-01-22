373e215788d93361de2e81fd6152f54804fec8f7
duvarakesh <duvarakesh05@gmail.com>
2025-01-23 00:24:14 +0530

    commit

--- TaskManger-Frontend/src/pages/Task.tsx
+++ TaskManger-Frontend/src/pages/Task.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "../components/navbar";
+import Navbar from "/src/components/Navbar";
 
 const Task = () => {
   const [tasks, setTasks] = useState([
